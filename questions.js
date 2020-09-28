812ca36f628b26db0bcf175f251de714898f9614
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-09-28 17:52:41 -0500

    added a data index to the choices

--- questions.js
+++ questions.js
@@ -2,11 +2,11 @@
 var questions = {
     "1": {
        "title": "What holds the codition in a for loop?",
-        "choices": ["Parenthesis","Quotes", "Angle Brackets", "Angle Brakets" ]
+        "choices": ["Parenthesis","Quotes", "Square Brackets", "Angle Brakets" ]
     },
     "2": {
         "title": "Commonly used data types DO NOT include:",
-        "choices": ["alerts", "Integers", "Booleans", "Strings"]
+        "choices": ["Alerts", "Integers", "Booleans", "Strings"]
     } 
 }
 
@@ -19,20 +19,40 @@ start.addEventListener("click", function(event){
     if (event.target.matches("button")){
         startPage.setAttribute("class", "hide");
         questionPage.removeAttribute("class");
+        renderQuestion(1);
     }
 
 })
 
 // this function will render a new question, given the number of the question
 function renderQuestion(i) {
+    // first the question title is rendered
     var questionTitle = document.querySelector(".question-title");
-    var choiceDiv = document.querySelector(".choices");
     questionTitle.textContent = questions[i].title;
+
+    var choiceDiv = document.querySelector(".choices");
+
+    // choicePosition is used to randomize the answers. a random number is chosen
+    // that number will be index of the first answer
+    // then the number is taken off to not be used again
+    /* the button is created with the answer as the text */
+
+    var choicePosition=["0", "1", "2", "3"];
     for (var j=0; j<questions[i].choices.length; j++){
+        var randomIndex = Math.floor(Math.random()*choicePosition.length);
         var buttonChoice = document.createElement("button");
-        buttonChoice.textContent=questions[i].choices[j];
-        buttonChoice.setAttribute("data-index", j);
+        buttonChoice.textContent=(j+1) + ". " + questions[i].choices[choicePosition[randomIndex]];
+        buttonChoice.setAttribute("data-index", questions[i].choices[choicePosition[randomIndex]]);
         choiceDiv.appendChild(buttonChoice);
+        var lineBreak = document.createElement("br");
+        choiceDiv.appendChild(lineBreak);
+        choicePosition.splice(randomIndex, 1);
+
     }
 }
 
+
+
+function startTimer() {
+
+}
