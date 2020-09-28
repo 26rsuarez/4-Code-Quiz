700f0b0cf1f34a3ec7ff2ab61f90f2a08ce09b8f
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-09-27 19:00:27 -0500

    added a js file for the questions

---
+++ questions.js
@@ -0,0 +1,14 @@
+var questions = {
+    "question1": {
+       "title": "What holds the codition in a for loop",
+        "correctAnswer": "Parenthesis",
+        "incorrectAnswer": ["Quotes", "Angle Brackets", "Angle Brakets" ]
+    },
+    "question2": {
+        "title": "Commonly used data types DO NOT include:",
+        "correctAnswer": "alerts",
+        "incorrectAnswer": ["Integers", "Booleans", "Strings"]
+    } 
+}
+
+
